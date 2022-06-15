0a39deb97ca7d2bdab57a0d232c77bf1971e251e
Xerioss <101500351+xerioss8448@users.noreply.github.com>
2022-06-15 19:08:08 +0200

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+sudo pip install requests
+sudo pip install argparse 
+sudo cp dirbrute.py /bin/dirbrute
+sudo chmod 777 /bin/dirbrute
+
+
+echo "[+] The installation was successfully completed"
